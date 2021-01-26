08ee5eaa403ff57da0e5f912a610bfb92ca40ccc
anshulbhatt29 <75416269+anshulbhatt29@users.noreply.github.com>
2021-01-26 19:27:13 +0530

    Update index.php
    Changes are mode

--- index.php
+++ index.php
@@ -1,60 +1,63 @@
-<?php
-if (isset($_POST['submit'])) {
-    $interest = $_POST['interest'];
-    if (sizeof($interest) < 3||sizeof($interest)>5) {
-        echo "At least select any 3 and at max 5 Interests.";}}
-
-    ?>
 <!DOCTYPE html>
 <html>
-<head>
-<script>
-function validateForm() {
-  var k = document.forms["MY FORM"]["name"].value;
-  if (k == "") {
-    alert("Name must be filled out");
-    return false;
-  }
-  var l = document.forms["MYFORM"]["contact"].value;
-  if (l.length!=10) {
-    alert("length must be 10");
-    return false;
-  }
-  
-}
-</script>
-</head>
+<styles>
+        table,th,td{
+            border:1px solid black;
+        }
+</styles>
 <body>
 
-<form name="MyFORM" action="test.php" onsubmit="return validateForm()" method="post">
-  Name: <input type="text" name="name"><br>
-  EMAIL<input type="email" name="email" required ><br>
-  CONTACT<input type="text" name="contact" ><br>
-  CITY<select  name="City" >
-                 <option>Dehradun</option>
-                 <option>Pune</option>
-                 <option>Mumbai</option>
-                 <option>Bangalore</option>
-                 <option>Others</option>
-                 </select><br>
-        COURSE<select name="Course" >
-               <option>BTECH</option>
-                 <option>BCA</option>
-                 <option>MCA</option>
-                 <option>Others</option>
-                 </select><br>
-                 INTERESTS
-        Coding<input type="checkbox" name="interest[]" value="Programming">
-        cricket<input type="checkbox" name="interest[]" value="Cricket">
-        Fotball<input type="checkbox" name="interest[]" value="Football">
-        Watching<input type="checkbox" name="interest[]" value="Reading">
-        Swimminginput type="checkbox" name="interest[]" value="Singing">
-        Walking<input type="checkbox" name="interest[]" value="Paiting">
-        <br>
-
-
-  <input type="submit" value="Submit">
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Contact</th>
+            <th>City</th>
+            <th>Course</th>
+            <th>Interests</th>
+</tr>
+<tr>
+    <td> <?php echo $_POST["Name"]; ?></td>
+    <td> <?php echo $_POST["Email"]; ?></td>
+    <td> <?php echo $_POST["contact"]; ?></td>
+    <td> <?php echo $_POST["city"]; ?></td>
+    <td> <?php echo $_POST["course"]; ?></td>
+</tr>
+</table>
+<form action="inde.php" method="post">
+<label>NAME:</label><br>
+<input type="text" name="Name" required><br>
+<label>E-Mail:</label><br>
+<input type="email" name="Email" required><br>
+<label>Contact_No:</label><br>
+<input type="tel" name="contact" required><br>
+<label>City:</label><br>
+<select name="city" required>
+    <option value="Dehradun">Dehradun</option>
+    <option value="Pune">Pune</option>
+    <option value="Mumbai">Mumbai</option>
+    <option value="Other">Other</option>
+</select><br>
+<label>course:</label><br>
+<select name="course" required>
+    <option value="BTECH">BTECH</option>
+    <option value="IT">IT</option>
+    <option value="BSC">BSC</option>
+    <option value="BCOM">BCOM</option>
+    </select><br>
+    <label >Interests:</label><br>
+    <input type="checkbox" name="Interests1" value="Cricket">
+    <label>Cricket</label>
+    <input type="checkbox" name="Interests2" value="Coding">
+    <label>Coding</label>
+    <input type="checkbox" name="Interests3" value="Anime">
+    <label>Anime</label>
+    <input type="checkbox" name="Interests4" value="Music">
+    <label>Music</label>
+    <input type="checkbox" name="Interests5" value="Imagining">
+    <label>Imagining</label>
+    <label>Submit</label><br>
+    <input type="submit" name="submit">
 </form>
-
 </body>
 </html>
